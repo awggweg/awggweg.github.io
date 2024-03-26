6da8663ca34b1e367184b2a9b86a1b93d21c0655
awggweg <15680317545@163.com>
2024-03-26 14:04:53 +0000

    ok UTC 2024-03-26 Tuesday 14:04:53 Updated By Github Actions

---
+++ js/som.js
@@ -0,0 +1,30 @@
+
+
+var pic = [];
+    
+    pic[1] = "https://s2.loli.net/2024/02/08/SaGcjgiXvANOIoq.webp";
+    pic[2] = "https://s2.loli.net/2024/02/08/qQHZYgzMDcUjyeC.webp";
+    pic[3] = "https://s2.loli.net/2024/02/08/qQHZYgzMDcUjyeC.webp";
+    pic[4] = "https://s2.loli.net/2024/02/08/34J9wdjEWurqOpB.webp";
+    pic[5] = "https://s2.loli.net/2024/02/08/jcdmQTWrRYy7o9V.webp";
+    pic[6] = "https://s2.loli.net/2024/02/08/IypurGlekjdgKMa.webp";
+    pic[7] = "https://s2.loli.net/2024/02/08/IypurGlekjdgKMa.webp";
+    pic[8] = "https://s2.loli.net/2024/02/08/CFBtY4xzLOcqvRU.webp";
+    pic[9] = "https://s2.loli.net/2024/02/08/CFBtY4xzLOcqvRU.webp";
+    pic[10] = "https://s2.loli.net/2024/02/08/Q8qdawhZgOcILSM.webp";
+    pic[11] = "https://s2.loli.net/2024/02/08/3LvxbTscwSCpUth.webp";
+    pic[12] = "https://s2.loli.net/2024/02/08/3LvxbTscwSCpUth.webp";
+    pic[13] = "https://s2.loli.net/2024/02/08/Bq7QOMuLPzh8GJ1.webp";
+    pic[14] = "https://s2.loli.net/2024/02/08/szJWBEYIhkD4aP6.webp";
+    pic[15] = "https://s2.loli.net/2024/02/08/vN8W9um1YpkGU3c.webp";
+    pic[16] = "https://s2.loli.net/2024/02/08/vN8W9um1YpkGU3c.webp";
+    pic[17] = "https://s2.loli.net/2024/02/08/OcJjHlRnDwuU745.webp";
+    pic[18] = "https://s2.loli.net/2024/02/08/Bq7QOMuLPzh8GJ1.webp";
+    pic[19] = "https://s2.loli.net/2024/02/08/u9CVGJXhS5wRAFj.webp";
+    pic[0] = "https://s2.loli.net/2024/02/08/CMGYcuhLyvPpsNm.webp";
+    var randomBgIndex = Math.round( Math.random() * 19 );
+
+    //输出随机的背景图
+    document.write('<style>body{background:url(' + pic[randomBgIndex] + ') no-repeat 50% 0; background-size: cover; background-attachment: fixed;}</style>');
+  
+    
